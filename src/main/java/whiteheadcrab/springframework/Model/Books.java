860f7d388f0d2ca03953c701ca67c927a90bc76b
Whiteheadcrab <lettersorstudy@gmail.com>
2021-07-26 17:55:55 +0200

    Version 0.2 Added model for table "books"

---
+++ src/main/java/whiteheadcrab/springframework/Model/Books.java
@@ -0,0 +1,80 @@
+package whiteheadcrab.springframework.Model;
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "books")
+public class Books
+{
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NotNull
+    private String bookName;
+
+    @NotNull
+    private String authorName;
+
+    @NotNull
+    private String isbn;
+
+    public Books(Long id, String bookName, String authorName, String isbn)
+    {
+        this.id = id;
+        this.bookName = bookName;
+        this.authorName = authorName;
+        this.isbn = isbn;
+    }
+
+    public Books()
+    {
+        super();
+    }
+
+
+    public Long getId()
+    {
+        return id;
+    }
+
+    public void setId(Long id)
+    {
+        this.id = id;
+    }
+
+    public String getBookName()
+    {
+        return bookName;
+    }
+
+    public void setBookName(String bookName)
+    {
+        this.bookName = bookName;
+    }
+
+    public String getAuthorName()
+    {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName)
+    {
+        this.authorName = authorName;
+    }
+
+    public String getIsbn()
+    {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn)
+    {
+        this.isbn = isbn;
+    }
+}
