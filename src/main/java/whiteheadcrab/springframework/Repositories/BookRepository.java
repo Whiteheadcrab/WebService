1ba3615dbfc6b18240d5d415a54b2eed2e126372
Whiteheadcrab <lettersorstudy@gmail.com>
2021-07-27 20:59:08 +0200

    Version 0.3 Added repository for table "books"

---
+++ src/main/java/whiteheadcrab/springframework/Repositories/BookRepository.java
@@ -0,0 +1,10 @@
+package whiteheadcrab.springframework.Repositories;
+
+import whiteheadcrab.springframework.Model.Books;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository
+{
+}
